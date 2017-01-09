c4e615b6f8d7677574a1a630cdffc565dfc6c06d
Nico Kemnitz <nkemnitz@princeton.edu>
2017-01-08 22:22:45 -0500

    Initial commit

--- WebGL2/app.ts
+++ WebGL2/app.ts
@@ -1,28 +1,14 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿window.onload = () => {
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+    const CANVAS: HTMLCanvasElement = document.getElementById("webglcanvas") as HTMLCanvasElement;
+    const GL: WebGL2RenderingContext = CANVAS.getContext("webgl2") as any;
+
+    if (!GL) {
+        console.error("WebGL 2.0 context creation failed.");
+        return;
     }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
+    let t: WebGLProgram = new WebGLShader();
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    console.log("WebGL 2.0 context successfully created.");
 };
